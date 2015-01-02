987d5fe4814f12dbe1829b359750bf6def9b9ad7
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2015-01-02 15:19:57 +0000

    add tests

--- src/Payum/Core/Bridge/Symfony/Form/Type/PaymentConfigType.php
+++ src/Payum/Core/Bridge/Symfony/Form/Type/PaymentConfigType.php
@@ -5,12 +5,9 @@ use Payum\Core\PaymentFactoryInterface;
 use Payum\Paypal\ExpressCheckout\Nvp\PaymentFactory as PaypalExpressCheckoutPaymentFactory;
 use Payum\Stripe\JsPaymentFactory as StripeJsPaymentFactory;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -23,7 +20,7 @@ class PaymentConfigType extends AbstractType
     protected $factories = array();
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -81,6 +78,16 @@ class PaymentConfigType extends AbstractType
                 'required' => $isRequired,
             ));
         }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Payum\Core\Model\PaymentConfigInterface'
+        ));
     }
 
     /**
